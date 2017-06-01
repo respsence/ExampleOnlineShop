ec865e21db5c2090c40cfca497a3811054f39dee
RespSence <negmetov@gmail.com>
2017-06-01 22:35:56 +0300

    first model, admin, form

--- ExampleOnlineShop/urls.py
+++ ExampleOnlineShop/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+admin.autodiscover()
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('landing.urls')),
--- landing/views.py
+++ landing/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
+from .forms import SubscriberForm
 
 def landing(request):
     name = 'Rustam'
     current_date = '30.05.2017'
+    form = SubscriberForm(request.POST or None)
+    if request.method == "POST":
+        print(form)
+
     return render(request, 'landing/landing.html',locals())

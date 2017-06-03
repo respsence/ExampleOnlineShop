ce4250b85d424d23736686c8428d180ce14ec2df
RespSence <negmetov@gmail.com>
2017-06-03 21:04:56 +0300

    form.save

--- landing/views.py
+++ landing/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
 from .forms import SubscriberForm
 
 def landing(request):
     name = 'Rustam'
     current_date = '30.05.2017'
     form = SubscriberForm(request.POST or None)
-    if request.method == "POST":
-        print(form)
+    if request.method == "POST" and form.is_valid():
+        print(request.POST)
+        data = form.cleaned_data
+        print(data["name"])
+
+        new_form = form.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
     return render(request, 'landing/landing.html',locals())

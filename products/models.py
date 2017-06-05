c47aac7b3bdc48b3ce783afc2115f2b15df7604b
RespSence <negmetov@gmail.com>
2017-06-05 22:07:14 +0300

    update settings.py media_url

--- products/models.py
+++ products/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, blank=True, null=True, default=None)
-    image = models.ImageField(upload_to='media/products_images/')
+    image = models.ImageField(upload_to='products_images/')
     is_active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)

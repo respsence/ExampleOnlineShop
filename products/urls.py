68c9c0905592981e6e3b8b9d6c8d728e9b3b7205
RespSence <negmetov@gmail.com>
2017-06-10 13:01:32 +0300

    add page product.html

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from landing import views
+from products import views
 
 urlpatterns = [
-    # url(r'^landing/', views.landing, name='landing'),
+     url(r'^product/(?P<product_id>\w+)/$', views.product, name='product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,1 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect
+from products.models import Product
+
+def product(request, product_id):
+    product = Product.objects.get(id = product_id)
+    return render(request, 'products/product.html', locals())

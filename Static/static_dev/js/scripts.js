d82100c52edcf84232886bbad1d46d99a6a679b9
RespSence <negmetov@gmail.com>
2017-06-11 20:17:00 +0300

    edit navbar add basket-items

--- Static/static_dev/js/scripts.js
+++ Static/static_dev/js/scripts.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
         var submit_btn = $('#submit_btn');
         var product_id = submit_btn.data('product_id');
         var product_name = submit_btn.data('name');
+        var product_price = submit_btn.data('price');
         console.log(product_id);
         console.log(product_name);
     })
